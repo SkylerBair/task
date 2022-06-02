d98f84129123f138195572d54f8d677de80995c0
skyler bair <skylertimothybair@hotmail.com>
2022-06-02 13:59:40 -0600

    completed standard project and starting on the bonus item

--- cmd/list.go
+++ cmd/list.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/SkylerBair/task/db"
 	"github.com/spf13/cobra"
 )
 
@@ -11,20 +13,23 @@ var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "List all of your tasks.",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("list called")
+		tasks, err := db.AllTasks()
+		if err != nil {
+			fmt.Println("Something went wrong:", err.Error())
+			os.Exit(1)
+		}
+		if len(tasks) == 0 {
+			fmt.Println("You have no tasks to complete! Why not take a vacation? ")
+			return
+		}
+
+		fmt.Println("You have yher following tasks:")
+		for i, task := range tasks {
+			fmt.Printf("%d, %s\n", i+1, task.Value)
+		}
 	},
 }
 
 func init() {
 	RootCmd.AddCommand(listCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// listCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// listCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "github.com/SkylerBair/task/cmd"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/SkylerBair/task/cmd"
+	"github.com/SkylerBair/task/db"
+	homedir "github.com/mitchellh/go-homedir"
+)
 
 func main() {
+	home, _ := homedir.Dir()
+	dbPath := filepath.Join(home, "tasks.db")
+	err := db.Init(dbPath)
+	if err != nil {
+		panic(err)
+	}
 	cmd.RootCmd.Execute()
+}
+
+func must(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 }
